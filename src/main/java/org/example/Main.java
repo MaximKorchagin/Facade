9c1c73b766c90edf52980027c8e907a2b677b053
MaximKorchagin <Airfallsc2@gmail.com>
2023-08-24 20:20:32 +0300

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("100011", "101101"));
+        System.out.println(binOps.mult("100011", "101101"));
+
+    }
+}
